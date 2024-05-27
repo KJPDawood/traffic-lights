8fb78fb642dbc1c3d06804b1a28bdba0fbcaec41
KJPDawood <kjpdawood@gmail.com>
2024-05-27 15:41:40 -0700

    first and last comment

---
+++ setting.js
@@ -0,0 +1,26 @@
+var btn = document.getElementById("btn")
+var red = document.getElementById("red")
+var yellow = document.getElementById("yellow")
+var green = document.getElementById("green")
+count = true
+btn.addEventListener("click" ,function(){
+    if (count === true) {
+        btn.innerHTML = "Stop"
+        red.style.backgroundColor = "red"
+        count = false
+        setTimeout(() => {
+            red.style.backgroundColor = "rgb(70, 17, 17)"
+            yellow.style.backgroundColor = "yellow"
+        }, 15000);
+        setTimeout(() => {
+            yellow.style.backgroundColor = "rgb(85, 85, 16)"
+            green.style.backgroundColor = "green"
+        }, 20000);
+    }else if (count === false) {
+        btn.innerHTML = "Start"
+        yellow.style.backgroundColor = "rgb(85, 85, 16)"
+        green.style.backgroundColor = "rgb(10, 58, 10)"
+        red.style.backgroundColor = "rgb(70, 17, 17)"
+        count = true
+    }
+})
